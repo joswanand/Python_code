6659146cfb1b3f5476f6fa19c31feefb1bb3dfd1
joswanand <112314570+joswanand@users.noreply.github.com>
2024-05-08 19:48:55 +0530

    Update sj.py

--- sj.py
+++ sj.py
@@ -1,7 +1,7 @@
 # This program adds two numbers
-num1 = 1.5
-num2 = 6.3
+num1 = 10
+num2 = 20
 # Add two numbers
 sum = num1 + num2
 # Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum of two number is:'sum)
